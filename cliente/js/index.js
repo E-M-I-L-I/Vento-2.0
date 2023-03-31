5ec48d1d1edae7b56dd470b5a83788e56d4c302f
E-M-I-L-I <emili.z@aluno.ifsc.edu.br>
2023-03-31 12:16:46 +0000

    Programar transição de cenas

---
+++ cliente/js/index.js
@@ -0,0 +1,23 @@
+import config from "./config.js";
+
+import abertura from "./cena-abertura.js";
+import principal from "./cena-principal.js";
+import fim_do_jogo from "./cena-fim-do-jogo.js";
+import final_feliz from "./cena-final-feliz.js";
+
+class Game extends Phaser.Game {
+    constructor() {
+        super(config);
+
+        this.scene.add("abertura", abertura);
+        this.scene.add("principal", principal);
+        this.scene.add("fim-do-jogo", fim_do_jogo);
+        this.scene.add("final-feliz", final_feliz);
+
+        this.scene.start("abertura");
+    }
+}
+
+window.onload = () => {
+    window.game = new Game();
+};
---
+++ servidor/servidor.js
@@ -0,0 +1,6 @@
+const express = resquire("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("../cliente/"));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
